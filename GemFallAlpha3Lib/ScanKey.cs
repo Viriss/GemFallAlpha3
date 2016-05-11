38ef6338a3724c2dbf5bc2db1b64f1ab65e1a40e
andrew <viriss@gmail.com>
2016-05-11 18:06:29 -0500

    make library project

--- GemFallAlpha3Lib/ScanKey.cs
+++ GemFallAlpha3Lib/ScanKey.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GemFallAlpha3
+﻿
+namespace GemFallAlphaLib
 {
     public struct ScanKey
     {
